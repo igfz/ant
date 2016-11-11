27cd771892c624492d93c699dff1f8260b66b5ee
kt.zhao@8win.com <kt.zhao@8win.com>
2016-11-11 17:20:12 +0800

    复制功能

--- src/components/ApplicationCenter/AppSet/AppKey/AppKey.js
+++ src/components/ApplicationCenter/AppSet/AppKey/AppKey.js
@@ -5,9 +5,6 @@ import {Link } from 'react-router'
 import CopyToClipboard from 'react-copy-to-clipboard';
 
 export default class AppKey extends Component {
-	getInitialState() {
-    return {value: '', copied: false};
-  }
 	render() {
 		return (
 			<div>
@@ -16,21 +13,13 @@ export default class AppKey extends Component {
 					<p>APP ID</p>
 					<Row type="flex" justify="space-between">
 						<Col xs={24} sm={24} md={17} lg={17}>
-							<input value={this.state.value}
-          onChange={({target: {value}}) => this.setState({value, copied: false})} />
-
-          <CopyToClipboard text={this.state.value}
-          onCopy={() => this.setState({copied: true})}>
-          <span>Copy to clipboard with span</span>
-        </CopyToClipboard>
-
-
-        {this.state.copied ? <span style={{color: 'red'}}>Copied.</span> : null}
-
-        
 							<Input defaultValue="48HGHKDBE28HJ28HH2FFB-GT33" readOnly/>
 						</Col>
-						<Col xs={24} sm={24} md={3} lg={3}><Button type="ghost">复制</Button></Col>
+						<Col xs={24} sm={24} md={3} lg={3}>
+							<CopyToClipboard text="48HGHKDBE28HJ28HH2FFB-GT33">
+							  <Button type="ghost">复制</Button>
+							</CopyToClipboard>
+						</Col>
 						<Col xs={0} sm={0} md={3} lg={3}></Col>
 					</Row>
 				</div>
@@ -40,7 +29,11 @@ export default class AppKey extends Component {
 						<Col xs={24} sm={24} md={17} lg={17}>
 							<Input defaultValue="48HGHKDBE28HJ28HH2FFB-GT33" readOnly/>
 						</Col>
-						<Col xs={24} sm={24} md={3} lg={3}><Button type="ghost">复制</Button></Col>
+						<Col xs={24} sm={24} md={3} lg={3}>
+							<CopyToClipboard text="48HGHKDBE28HJ28HH2FFB-GT334">
+							  <Button type="ghost">复制</Button>
+							</CopyToClipboard>
+						</Col>
 						<Col xs={0} sm={0} md={3} lg={3}></Col>
 					</Row>
 				</div>
@@ -52,7 +45,9 @@ export default class AppKey extends Component {
 						</Col>
 						<span className={styles.tip}> Master Key是最高权限的Key，一旦泄漏请立即使用 【重置】 按钮进行重置</span>
 						<Col xs={11} sm={11} md={3} lg={3}>
-							<Button type="ghost">复制</Button>
+							<CopyToClipboard text="48HGHKDBE28HJ28HH2FFB-GT35">
+							  <Button type="ghost">复制</Button>
+							</CopyToClipboard>
 						</Col>
 						<Col xs={11} sm={11} md={3} lg={3}>
 							<Button type="ghost">重置</Button>
