ace499b372d9e545da484d55d3c3e5d5196fe9dc
fl.zhang@8win.com <zhangfaliang>
2016-11-16 14:40:16 +0800

    支付成功小尺寸修改

--- src/components/UserCenter/AccountInfo/Recharge/PaySuccess.js
+++ src/components/UserCenter/AccountInfo/Recharge/PaySuccess.js
@@ -13,30 +13,30 @@ class PaySuccess extends Component{
 	}
 	render(){
 		return(
-			<Row className={styles['r_wrap']} >
-				<Row className={styles['pay_suc']}>
-					<Col lg={24}>
-						支付成功！
-					</Col>
-				</Row>
-				<Row>
-					<Col lg={24} md={24} sm={24} xs={24} >
-						订单号：56156486515
-					</Col>
-				</Row>
-				<Row>
-					<Col lg={24} md={24} sm={24} xs={24} >
-						订单金额：54588
-					</Col>
-				</Row>
-				<Row className={styles['btn_col']}>
-					<Col lg={24} md={24} sm={24} xs={24} >
-						<Button className={styles['btn_re']}>
-						 <Link to="/usercenter/accountinfo">返回账号信息</Link>
-						</Button>
-					</Col>
-				</Row>
-			</Row>
+			<div  className={styles['success']}>
+					<Row className={styles['pay_suc']}>
+						<Col lg={24} md={24} sm={24} xs={24}>
+							支付成功！
+						</Col>
+					</Row>
+					<Row className={styles['order_num']}>
+						<Col lg={24} md={24} sm={24} xs={24} >
+							订单号：56156486515
+						</Col>
+					</Row>
+					<Row className={styles['order_price']}>
+						<Col lg={24} md={24} sm={24} xs={24} >
+							订单金额：54588
+						</Col>
+					</Row>
+					<Row className={styles['btn_col']}>
+						<Col lg={24} md={24} sm={24} xs={24} >
+							<Button className={styles['btn_re']}>
+							 <Link to="/usercenter/accountinfo">返回账号信息</Link>
+							</Button>
+						</Col>
+					</Row>
+			</div>
 			)
 	}
 }
