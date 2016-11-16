3e67f1c96b345e82ffb3f5147bf13d8e871f9041
fl.zhang@8win.com <zhangfaliang>
2016-11-16 13:45:17 +0800

    充值小尺寸修改

--- src/components/UserCenter/AccountInfo/Recharge/Recharge.js
+++ src/components/UserCenter/AccountInfo/Recharge/Recharge.js
@@ -13,50 +13,47 @@ class Recharge extends Component{
 	}
 	render(){
 		return(
-			<Row className={styles['r_wrap']} >
-				<div  className={styles['center_wrap']} >
-					<Row className={styles['pay']}>
-					<Col lg={24} md={24} sm={24} xs={24}>
-						充值
-					</Col>
-				</Row>
-				<Row className={styles['pay_mode']}>
-					<Col lg={24} md={24} sm={24} xs={24}>
-						<Button>支付宝</Button>
-						<Button className={styles['wechat']}>微信</Button>
-					</Col>
-				</Row>
-				<Row className={styles['pay_input']}>
-					<Col lg={5} md={5} sm={5} xs={5}>
-							充值金额
-					</Col>
-					<Col lg={19} md={19} sm={19} xs={19}>
-						<Input placeholder="Basic usage" />
-					</Col>
-				</Row>
-				<Row className={styles['btn_col']}>
-					<Col lg={24} md={24} sm={24} xs={24} >
-						<Button className={styles['btn_re']}>
-						 <Link to='/usercenter/paysuccess'>确定充值 </Link>
-						</Button>
-					</Col>
-				</Row>
-				<Row  className={styles['bank_info']}>
-					<Col lg={24} md={24} sm={24} xs={24} >
-						开户行：尔特人特瑞特人<br/>
-						开户行：尔特人特瑞特人<br/>
-						开户行：尔特人特瑞特人<br/>
-						开户行：尔特人特瑞特人<br/>
-						开户行：尔特人特瑞特人<br/>
-					</Col>
-				</Row>
-				<Row >
-					<Col lg={24} md={24} sm={24} xs={24} >
-					可以通过微信充值可以通过微信充值可以通过微信充值可以通过微信充值
-					</Col>
-				</Row>
+				<div className={styles['center_wrap']}>
+					<Row className={styles['rechar_text']} >
+					 <Col lg={24} md={24} sm={24} xs={24}>
+					 		<span >充值</span>
+					 </Col>
+					</Row>
+					<Row className={styles['wrap_btn']} >
+					 <Col lg={11} md={11} sm={24} xs={24}>
+					 		<Button className={styles['alipay_btn']}>支付宝</Button>
+					 </Col>
+					 <Col lg={11} md={11} sm={24} xs={24}>
+					 		<Button className={styles['wechat_btn']}>微信</Button>
+					 </Col>
+					</Row>
+					<Row className={styles['recharge']} >
+					 <Col lg={4} md={4} sm={4} xs={4}>
+					 	充值金额
+					 </Col>
+					 <Col lg={20} md={20} sm={20} xs={20}>
+					 		<Input placeholder="请输入充值金额"></Input>
+					 </Col>
+					</Row>
+					<Row className={styles['wrap_sum']} >
+						<Col lg={24} md={24} sm={24} xs={24}>
+							<Button>确定支付</Button>
+						</Col>
+					</Row>
+					<Row className={styles['person_detail']} >
+						<Col lg={24} md={24} sm={24} xs={24}>
+							开户行：哈迪斯感觉<br/>
+							银行账号：56131231322<br/>
+							账户名称：章鱼彩票<br/>
+							开户行信息：章鱼彩票
+						</Col>
+					</Row>
+					<Row className={styles['explain']} >
+						<Col lg={24} md={24} sm={24} xs={24}>
+							可以通过对公付款充值，汇款附言里请务必写明用户名或者绑定手机号
+						</Col>
+					</Row>
 				</div>
-			</Row>
 			)
 	}
 }
