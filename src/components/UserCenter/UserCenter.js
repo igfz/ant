86133c2cd1fea3f63e23fd82ad018d30a78afc77
fl.zhang@8win.com <zhangfaliang>
2016-11-16 16:21:52 +0800

    个人中心小尺寸样式修改

--- src/components/UserCenter/UserCenter.js
+++ src/components/UserCenter/UserCenter.js
@@ -25,7 +25,7 @@ export default function UserCenter({ children }){
 
 			<Banner />
 
-			<div style={{'maxWidth':'1200px','minHeight':'1200px','margin':'3% auto'}}>
+			<div className={styles['ctr_style']} >
 				<Row type="flex" justify="center">
 					<Col xs={22} sm={22} md={5} lg={5}>
 
