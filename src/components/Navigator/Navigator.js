2893a7f8a205ad136af6aee87b0b0ea8460de3b2
kt.zhao@8win.com <kt.zhao@8win.com>
2016-11-17 13:53:45 +0800

    导航数据补齐，样式调整

--- src/components/Navigator/Navigator.js
+++ src/components/Navigator/Navigator.js
@@ -19,21 +19,21 @@ const data=[
 							"data":[
 								{
 									"key":"1",
-									"title":"彩票服务",
-									"describe":"多国家顶尖博彩公司实时赔率盘口总汇，先人一步拿到最新动态多国家顶尖博彩公司实时赔率盘口总汇，先人一步拿实时赔率盘口总汇，先人一步拿到最新动态",
-									"route":"/documents/detail"
+									"title":"竞彩足篮球",
+									"describe":"提供竞彩足篮球的全部赛程和固定奖金服务，玩法全面",
+									"route":""
 								},
 								{
 									"key":"2",
-									"title":"赔率",
-									"describe":"多国家顶尖博彩公司实时赔率盘口总汇",
-									"route":"/documents/detail"
+									"title":"传统足彩",
+									"describe":"提供传统足彩的所有期次赛程和开奖信息服务，玩法全面",
+									"route":""
 								},
 								{
 									"key":"3",
-									"title":"彩票服务3",
-									"describe":"多国家顶尖博彩公司实时赔率盘口总汇，先人一步拿到最新动态多国家顶尖博彩公司实时赔率盘口总汇，先人一步拿到最新动态多国汇，先人一步拿到最新动态",
-									"route":"/documents/detail"
+									"title":"北京单场",
+									"describe":"提供北京单场的所有期次赛程、浮动SP值和开奖SP值服务，玩法全面",
+									"route":""
 								}
 							]
 						},
@@ -43,21 +43,21 @@ const data=[
 							"data":[
 								{
 									"key":"4",
-									"title":"彩票服务",
-									"describe":"多国家顶尖博彩公司实时赔率盘口总汇，先人一步拿到最新动态多国家顶尖博彩公司实时赔率盘口总汇，先人一步拿实时赔率盘口总汇，先人一步拿到最新动态",
-									"route":"/documents/detail"
+									"title":"足篮球竞猜",
+									"describe":"提供实时的足篮球赛前盘/滚球盘的赛程、赔率和操盘服务",
+									"route":""
 								},
 								{
 									"key":"5",
-									"title":"彩票服务1",
-									"describe":"多国家顶尖博彩公司实时赔率盘口总汇",
-									"route":"/documents/detail"
+									"title":"电竞竞猜",
+									"describe":"提供实时电子竞技赛前盘/滚球盘的赛程、赔率和操盘服务",
+									"route":""
 								},
 								{
 									"key":"6",
-									"title":"彩票服务3",
-									"describe":"多国家顶尖博彩公司实时赔率盘口总汇，先人一步拿到最新动态多国家顶尖博彩公司实时赔率盘口总汇，先人一步拿到最新动态多国汇，先人一步拿到最新动态",
-									"route":"/documents/detail"
+									"title":"事件竞猜",
+									"describe":"提供多种事件竞猜的赔率和操盘服务，事件也可由用户定制",
+									"route":""
 								}
 							]
 						},
@@ -67,23 +67,105 @@ const data=[
 							"data":[
 								{
 									"key":"7",
-									"title":"彩票服务",
-									"describe":"多国家顶尖博彩公司实时赔率盘口总汇，先人一步拿到最新动态多国家顶尖博彩公司实时赔率盘口总汇，先人一步拿实时赔率盘口总汇，先人一步拿到最新动态",
-									"route":"/documents/detail"
+									"title":"赔率盘口",
+									"describe":"多家国外顶尖博彩公司实时赔率盘口汇总，先人一步拿到最新动态",
+									"route":""
 								},
 								{
 									"key":"8",
-									"title":"彩票服务1",
-									"describe":"多国家顶尖博彩公司实时赔率盘口总汇",
-									"route":"/documents/detail"
+									"title":"战绩交锋",
+									"describe":"球队历史战绩、交锋战绩、未来赛程安排一览无余",
+									"route":""
 								},
 								{
 									"key":"9",
-									"title":"彩票服务2",
-									"describe":"多国家顶尖博彩公司实时赔率盘口总汇，先人一步拿到最新动态多国家顶尖博彩公司实时赔率盘口总汇，先人一步拿到最新动态多国汇，先人一步拿到最新动态",
-									"route":"/documents/detail"
+									"title":"赛事排行",
+									"describe":"赛事排行榜、积分榜，更新及时数据准确",
+									"route":""
+								},
+								{
+									"key":"10",
+									"title":"出场阵容",
+									"describe":"首发阵容、阵型消息及时准确",
+									"route":""
+								},
+								{
+									"key":"11",
+									"title":"比分直播",
+									"describe":"比分、时间、场中事件直播，比视频更快更准",
+									"route":""
+								},
+								{
+									"key":"12",
+									"title":"章鱼爆料",
+									"describe":"最及时的独家原创爆料新闻，多角度深层次解析球赛",
+									"route":""
+								},
+								{
+									"key":"13",
+									"title":"分析师推荐",
+									"describe":"近20位分析师，每日提供客观细致的赛前预测",
+									"route":""
 								}
 							]
+						}
+					]
+				},
+				{
+					"id":"5",
+					"title":"预算",
+					"icon":"appstore",
+					"special":"0",
+					"data":[]
+				},
+				{
+					"id":"6",
+					"title":"文档中心",
+					"icon":"setting",
+					"special":"0",
+					"data":[
+						{
+							"key":"14",
+							"title":"彩票服务文档",
+							"route":""
+						},
+						{
+							"key":"15",
+							"title":"竞猜游戏文档",
+							"route":""
+						},
+						{
+							"key":"16",
+							"title":"数据资讯文档",
+							"route":""
+						},
+						{
+							"key":"17",
+							"title":"其他文档",
+							"route":""
+						}
+					]
+				},
+				{
+					"id":"7",
+					"title":"技术支持",
+					"icon":"setting",
+					"special":"0",
+					"data":[
+						{
+							"key":"18",
+							"title":"购买技术支持",
+							"route":""
+						},
+						{
+							"key":"19",
+							"title":"工单系统",
+							"route":""
+						},
+						{
+							"key":"20",
+							"title":"Bug汇报",
+							"route":""
 						}
 					]
 				}
@@ -94,7 +176,6 @@ function handleClick(e) {
 }
 
 const Sider = React.createClass({
-
   getInitialState() {
     return {
       current: '1',
@@ -103,6 +184,11 @@ const Sider = React.createClass({
   },
   handleClick(e) {
     console.log('Clicked: ', e);
+    //移动端路由关闭导航
+		let navbar = document.getElementById("navbar")
+		let body=document.getElementsByTagName("body")[0]
+	 	utils.addClassName(navbar,styles.dn)
+		utils.removeClassName(body,styles.pf)
     this.setState({ current: e.key });
   },
   onOpenChange(openKeys) {
@@ -127,6 +213,53 @@ const Sider = React.createClass({
     };
     return map[key] || [];
   },
+  renderNavServers(x){
+		if(x==0){
+			return (
+				<SubMenu key={"sub"+data[x].id} title={
+					<span>
+						<Icon type={data[x].icon} />
+						<span>{data[x].title}</span>
+					</span>}>
+        	{
+        		data[0]["data"].map((list,i)=>
+      				<SubMenu key={"sub"+list.id} title={list.title}>
+		          	{list["data"].map((item,idx)=>
+		      				<Menu.Item key={item.key}>
+		      					<Link to={item.route} className={styles.href}>{item.title}</Link>
+	      					</Menu.Item>
+								)}
+		          </SubMenu>
+						)
+					}
+        </SubMenu>
+			)
+		}else{
+	  	if(data[x]["data"].length>0){
+				return (
+					<SubMenu key={"sub"+data[x].id} title={<span>
+						<Icon type={data[x].icon} />
+						<span>{data[x].title}</span>
+					</span>}>
+						{data[x]["data"].map((item,idx)=>
+							<Menu.Item key={"setting:"+idx}>
+									<span>{item.title}</span>
+							</Menu.Item>
+						)}
+					</SubMenu>
+				)
+			}else{
+				return (
+					<SubMenu className={styles.iconnone} key={"sub"+data[x].id} title={<span>
+						<Icon type={data[x].icon} />
+						<span>{data[x].title}</span>
+					</span>}>
+					</SubMenu>
+				)
+			}
+		}
+
+  },
   
   render() {
     return (
@@ -139,34 +272,13 @@ const Sider = React.createClass({
 	        onOpenChange={this.onOpenChange}
 	        onClick={this.handleClick}
 	      >
-	        <SubMenu key="sub1" title={<span><Icon type="mail" /><span>产品与服务</span></span>}>
-	        	{
-	        		data[0]["data"].map((list,i)=>
-	      				<SubMenu key={"sub"+list.id} title={list.title}>
-			          	{list["data"].map((item,idx)=>
-			      				<Menu.Item key={item.key}>{item.title}</Menu.Item>
-									)}
-			          </SubMenu>
-							)
-						}
-	        </SubMenu>
-	        <SubMenu key="sub5" title={<span><Icon type="appstore" /><span >预算</span></span>}>
-	        	<Menu.Item key="10">Option 10</Menu.Item>
-	          <Menu.Item key="11">Option 11</Menu.Item>
-	          <Menu.Item key="12">Option 12</Menu.Item>
-	        </SubMenu>
-	        <SubMenu key="sub6" title={<span><Icon type="setting" /><span>文档中心</span></span>}>
-           	<Menu.Item key="13">Option 9</Menu.Item>
-	          <Menu.Item key="14">Option 10</Menu.Item>
-	          <Menu.Item key="15">Option 11</Menu.Item>
-	          <Menu.Item key="16">Option 12</Menu.Item>
-	        </SubMenu>
-	        <SubMenu key="sub7" title={<span><Icon type="setting" /><span>技术支持</span></span>}>
-	          <Menu.Item key="17">Option 6</Menu.Item>
-	          <Menu.Item key="18">Option 6</Menu.Item>
-	        </SubMenu>
+	        {this.renderNavServers(0)}
+	        {this.renderNavServers(1)}
+	        {this.renderNavServers(2)}
+	        {this.renderNavServers(3)}
+	        
 	      </Menu>
-	      <Button type="ghost" className={styles.regBtn}>免费试用</Button>
+	      <Button type="ghost" className={styles.regBtn} onClick={this.handleClick}>免费试用</Button>
       </div>
     );
   },
@@ -245,10 +357,11 @@ class Navigator extends Component {
 			)
 	}
 
-	renderNavServes(){
-		return (
-			<Menu onClick={handleClick} style={{ width: 200 }} mode="vertical">
-				{data[0]["data"].map((item,idx)=>
+	renderNavServes(x){
+		if(x==0){
+			return (
+				<Menu onClick={handleClick} style={{ width: 200 }} mode="vertical">
+					{data[x]["data"].map((item,idx)=>
 						<SubMenu key={"sub"+idx} title={<span>{item.title}</span>}>
 							{item["data"].map((items,idxs)=>
 					        <Menu.Item key={items.key}>
@@ -262,6 +375,33 @@ class Navigator extends Component {
 					)}
 				</Menu>
 			)
+		}else{
+			if(data[x]["data"].length>0){
+				return (
+					<Menu mode="horizontal">
+							<SubMenu key={data[x].id} title={<span>
+								<Link className={styles.navhref}>{data[x].title}</Link></span>}>
+								{data[x]["data"].map((item,idx)=>
+									<Menu.Item key={"setting:"+idx}>
+										<Link to={item.rout} className={styles.href}>
+											<span>{item.title}</span>
+										</Link>
+									</Menu.Item>
+								)}
+							</SubMenu>
+		      </Menu>
+				)
+			}else{
+				return (
+					<Menu mode="horizontal">
+							<SubMenu key={data[x].id} title={<span>
+								<Link className={styles.navhref}>{data[x].title}</Link></span>}>
+							</SubMenu>
+		      </Menu>
+				)
+			}
+		}
+
 	}
 
 	render() {
@@ -280,40 +420,20 @@ class Navigator extends Component {
 								<SubMenu title={<Link className={styles.navhref}><span>
 									{data[0].title}</span></Link>}>
 			            <div className={styles.menuserver} >
-			            	{this.renderNavServes()}
+			            	{this.renderNavServes(0)}
 			            </div>
 				        </SubMenu>
 			        </Menu>
 						</Col>
 						<Col xs={0} sm={0} md={3} lg={3}  className={styles.menugroup+' '+styles.menupub}>
-							<Menu mode="horizontal">
-								<SubMenu key="sub3" title={<span><Link className={styles.navhref}>预算</Link></span>}>
-								</SubMenu>
-							</Menu>
+							{this.renderNavServes(1)}
 						</Col>
 						<Col xs={0} sm={0} md={3} lg={3} className={styles.menugroup+' '+styles.menupub}>
-
-							<Menu mode="horizontal">
-									<SubMenu title={<Link to="/documents" className={styles.navhref}><span>文档中心</span></Link>}>
-					            <Menu.Item key="setting:1"><Link to="/documents/detail" className={styles.defaulthref}><span>彩票服务文档</span></Link></Menu.Item>
-					            <Menu.Item key="setting:2"><Link to="/documents/detail" className={styles.defaulthref}>竞猜游戏文档</Link></Menu.Item>
-					             <Menu.Item key="setting:3"><Link to="/documents/detail" className={styles.defaulthref}>数据资讯文档</Link></Menu.Item>
-					            <Menu.Item key="setting:4"><Link to="/documents/detail" className={styles.defaulthref}>其他文档</Link></Menu.Item>
-					        </SubMenu>
-				        </Menu>
-							
+							{this.renderNavServes(2)}
 						</Col>
 						<Col xs={0} sm={0} md={3} lg={3}  className={styles.menugroup+' '+styles.menupub} >
-
-							<Menu mode="horizontal">
-								<SubMenu title={<span><Link className={styles.navhref}>技术支持</Link></span>}>
-				            <Menu.Item key="setting:1">购买技术支持</Menu.Item>
-				            <Menu.Item key="setting:2">工单系统</Menu.Item>
-				             <Menu.Item key="setting:3">Bug汇报</Menu.Item>
-				        </SubMenu>
-			        </Menu>
+							{this.renderNavServes(3)}
 						</Col>
-
 						<Col xs={0} sm={0} md={1} lg={1}></Col>
 						<Col xs={0} sm={0} md={3} lg={3} className={styles.menugroup}>
 							{this.loginFlag(true)}		
@@ -335,7 +455,7 @@ class Navigator extends Component {
 					</Row>
 				</header>
 				
-				<div ref="navbar" className={styles.dn}>
+				<div id="navbar" ref="navbar" className={styles.dn}>
 					<Sider/>
 				</div>
 
